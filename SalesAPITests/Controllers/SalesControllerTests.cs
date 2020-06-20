4e9abf8d795068edca6cd95fea8b0280261f2c3b
deepa <deepacaroline@gmail.com>
2020-06-21 00:12:04 +0530

    Moq package added, Test modification in progress

--- SalesAPITests/Controllers/SalesControllerTests.cs
+++ SalesAPITests/Controllers/SalesControllerTests.cs
@@ -18,38 +18,44 @@ namespace SalesAPI.Controllers.Tests
     public class SalesControllerTests
     {        
         private SalesController controller;
-        private ISalesRepository _repository;
+        private Mock<ISalesRepository> mockRepo;       
 
-        public SalesControllerTests(ISalesRepository repository)
+        
+        [TestInitialize]
+        public void Initialize()
         {
-            _repository = repository;
-            controller = new SalesController(_repository);
-          
+            mockRepo = new Mock<ISalesRepository>();            
         }
 
+        //[TestMethod()]
+        //public void GetTest_Success()
+        //{
+        //    controller = new SalesController(mockRepo.Object)
+        //    { 
+        //    };
+        //    mockRepo.Setup(x => x.GetByID(2)).Returns(new Sale { invoiceID = 2, productID = 02, productName = "bbb", productQuantity = 8, totalPrice = 80 });
+           
+
+        //    var response = controller.Get();
+        //    Assert.IsNotNull(response);
+          
+        //}
         [TestMethod()]
-        public void GetTest()
-        {  
-            var response = controller.Get();
-            Assert.IsNotNull(response);
-          
-        }
-
         public void GetTestByID_Success()
         {
-            var mockRepo = new Mock<ISalesRepository>();
             mockRepo.Setup(x => x.GetByID(2)).Returns(new Sale { invoiceID = 2, productID = 02, productName = "bbb", productQuantity = 8, totalPrice = 80 });
-
-            var response = controller.Get(2);            
-            Assert.IsNotNull(response);
+            controller = new SalesController(mockRepo.Object);
+            
+            var response = controller.Get(2);
+            Assert.AreEqual(response.productName, "bbb");
         }
 
-        private List<Sale> GetTestSale()
-        {
-            var testSales = new List<Sale>();
-            testSales.Add(new Sale { invoiceID = 1, productID = 01, productName = "aaa", productQuantity = 5, totalPrice = 50 });
-            testSales.Add(new Sale { invoiceID = 2, productID = 02, productName = "bbb", productQuantity = 8, totalPrice = 80 });
-            return testSales;
-        }
+        //private List<Sale> GetTestSale()
+        //{
+        //    var testSales = new List<Sale>();
+        //    testSales.Add(new Sale { invoiceID = 1, productID = 01, productName = "aaa", productQuantity = 5, totalPrice = 50 });
+        //    testSales.Add(new Sale { invoiceID = 2, productID = 02, productName = "bbb", productQuantity = 8, totalPrice = 80 });
+        //    return testSales;
+        //}
     }
 }
